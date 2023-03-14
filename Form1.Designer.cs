5a54eb3bb0e194e1b90a1188e3863c20c8d4d582
Carlos Emilio Cabral <Carlos Emilio Cabral@DESKTOP-UBTP0PR>
2023-03-14 15:30:59 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ArregloUnidimensional
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblValorBuscado = new System.Windows.Forms.Label();
+            this.lstValores = new System.Windows.Forms.ListBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(52, 46);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(131, 64);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click_1);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(217, 46);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(123, 64);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click_1);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(384, 46);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(143, 64);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(562, 46);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(143, 64);
+            this.btnOrdenar.TabIndex = 3;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click_1);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(101, 149);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(82, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // lblValorBuscado
+            // 
+            this.lblValorBuscado.AutoSize = true;
+            this.lblValorBuscado.Location = new System.Drawing.Point(19, 152);
+            this.lblValorBuscado.Name = "lblValorBuscado";
+            this.lblValorBuscado.Size = new System.Drawing.Size(76, 13);
+            this.lblValorBuscado.TabIndex = 5;
+            this.lblValorBuscado.Text = "Valor Buscado";
+            // 
+            // lstValores
+            // 
+            this.lstValores.FormattingEnabled = true;
+            this.lstValores.Location = new System.Drawing.Point(217, 152);
+            this.lstValores.Name = "lstValores";
+            this.lstValores.Size = new System.Drawing.Size(232, 277);
+            this.lstValores.TabIndex = 6;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.Location = new System.Drawing.Point(488, 155);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(262, 286);
+            this.lblCantidad.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lstValores);
+            this.Controls.Add(this.lblValorBuscado);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblValorBuscado;
+        private System.Windows.Forms.ListBox lstValores;
+        private System.Windows.Forms.Label lblCantidad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArregloUnidimensional
+{
+    public partial class Form1 : Form
+    {
+        // Declarar el arreglo
+        int[] arreglo = new int[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click_1(object sender, EventArgs e)
+        {
+            // Generar valores aleatorios para el arreglo
+            Random rnd = new Random();
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = rnd.Next(1, 101);
+            }
+
+            MessageBox.Show("El arreglo se ha generado correctamente.", "Generar arreglo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnMostrar_Click_1(object sender, EventArgs e)
+        {
+            // Mostrar los valores del arreglo en el ListBox
+            lstValores.Items.Clear();
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                lstValores.Items.Add("Elemento " + i + ": " + arreglo[i]);
+            }
+
+            // Mostrar la cantidad de elementos del arreglo
+            lblCantidad.Text = "Cantidad de elementos: " + arreglo.Length;
+        }
+
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+            // Buscar un valor en el arreglo
+            int valorBuscado = int.Parse(txtValor.Text);
+            int indiceEncontrado = -1;
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == valorBuscado)
+                {
+                    indiceEncontrado = i;
+                    break;
+                }
+            }
+
+            if (indiceEncontrado != -1)
+            {
+                MessageBox.Show("El valor " + valorBuscado + " se encuentra en el índice " + indiceEncontrado + ".", "Buscar valor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El valor " + valorBuscado + " no se encuentra en el arreglo.", "Buscar valor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnOrdenar_Click_1(object sender, EventArgs e)
+        {
+            // Ordenar el arreglo de forma ascendente
+            Array.Sort(arreglo);
+
+            MessageBox.Show("El arreglo se ha ordenado correctamente.", "Ordenar arreglo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
+
